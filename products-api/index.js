1d837c34e8882dc241e22ccb2cf4cae0c5559541
Timothy Nguyen <shimoshixp@gmail.com>
2021-03-19 03:18:05 -0700

    Finish endpoint for product

--- products-api/index.js
+++ products-api/index.js
@@ -13,11 +13,40 @@ app.get('/', (req, res) => {
 });
 
 app.get('/products/:id', (req, res) => {
-  // const { id } = req.params;
+  const { id } = req.params;
+  const product = {};
 
-  queries.getProduct()
+  queries.getProduct(id)
     .then((results) => {
-      res.status(201).send(results);
+      const {
+        id,
+        name,
+        slogan,
+        description,
+        category,
+        default_price
+      } = results.rows[0];
+
+      product.id = id;
+      product.name = name;
+      product.slogan = slogan;
+      product.description = description;
+      product.category = category;
+      product.default_price = default_price;
+
+      return queries.getFeatures(id);
+    })
+    .then((results) => {
+      product.features = [];
+
+      results.rows.forEach((feature) => {
+        product.features.push({
+          feature: feature.feature,
+          value: feature.type,
+        })
+      });
+
+      res.status(201).send(product);
     })
     .catch((error) => {
       res.status(501).send(error);
--- products-api/queries.js
+++ products-api/queries.js
@@ -7,10 +7,20 @@ const pool = new Pool({
   port: config.PORT,
 });
 
-const getProduct = () => {
-  return pool.query(`SELECT * from products where id = 333`)
+const getProduct = (id) => {
+  return pool.query(`
+    SELECT * FROM products WHERE id = ${id}
+      LIMIT 1
+  `)
+}
+
+const getFeatures = (id) => {
+  return pool.query(`
+    SELECT * FROM features WHERE product_id = ${id}
+  `)
 }
 
 module.exports = {
   getProduct,
+  getFeatures,
 }
